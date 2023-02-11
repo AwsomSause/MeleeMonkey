87372520789a41c3117bec73e238bc737f7de8ed
AwsomSause <125075768+AwsomSause@users.noreply.github.com>
2023-02-10 17:04:02 -0800

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+namespace MonkoReanimated;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "Melee Monkey";
+
+    public const string Description = "Brings the power of fists";
+
+    public const string RepoOwner = "AwsomSause"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "MonkoReanimated"; // TODO add your repo name here, also in the download url in README.md
+}
